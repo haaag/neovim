bb7f03b6ad2643a101bf7bfb3558cc796135916c
haaag <haag_@tutanota.com>
2022-12-19 16:18:00 -0300

    feat(autocmd): General autocommands.

--- lua/me/autocmd.lua
+++ lua/me/autocmd.lua
@@ -1,4 +1,4 @@
--- autocmd.lua
+-- [[ autocmd.lua ]]
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -9,4 +9,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+
+local sxhkd_group = vim.api.nvim_create_augroup("SxhkdGroup", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = sxhkd_group,
+  pattern = "*sxhkdrc",
+  command = "!pkill -USR1 sxhkd",
+  desc = "Update binds when sxhkdrc is updated.",
+})
+
+local misc = vim.api.nvim_create_augroup("Misc", { clear = true })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = misc,
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "startuptime" },
+  command = [[nnoremap <buffer><silent> q :quit<CR>]],
+  desc = "Use 'q' to quit from common plugins",
 })
