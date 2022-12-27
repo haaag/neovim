343c398fef8e1eb1ee3fc4591de7129cb9e93c19
haaag <haag_@tutanota.com>
2022-12-27 11:45:07 -0300

    feat(mason-tools): Ensure tools are installed.

--- lua/me/lsp/mason.lua
+++ lua/me/lsp/mason.lua
@@ -1,23 +1,68 @@
--- [[ mason config ]]
+-- mason config
 
--- Setup mason so it can manage external tooling
-require('mason').setup()
+local M = {}
 
--- Enable the following language servers
--- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'bashls', 'jsonls' }
+M.install_servers = function()
+  local servers = {
+    "clangd",
+    "rust_analyzer",
+    "pyright",
+    "tsserver",
+    "sumneko_lua",
+    "bashls",
+    "jsonls",
+    -- "pylsp",
+  }
 
--- Ensure the servers above are installed
-require('mason-lspconfig').setup {
-  ensure_installed = servers,
+  -- Ensure the servers above are installed
+  require("mason-lspconfig").setup({
+    ensure_installed = servers,
+  })
+
+  local on_attach = require("me.lsp.config").on_attach
+  local capabilities = require("me.lsp.config").capabilities()
+
+  for _, lsp in ipairs(servers) do
+    require("lspconfig")[lsp].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end
+end
+
+M.tools = {
+  -- lua
+  "luacheck",
+  "stylua",
+  -- python
+  "black",
+  "isort",
+  "mypy",
+  "pylint",
+  "pyproject-flake8",
+  -- web
+  "prettierd",
+  "eslint_d",
+  -- shell
+  "shellcheck",
+  "shfmt",
 }
 
-local on_attach = require("me.lsp.config").on_attach
-local capabilities = require("me.lsp.config").capabilities()
-
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+-- Ensure the tools above are installed
+M.check_tools = function()
+  local registry_present, mason_registry = pcall(require, "mason-registry")
+  if registry_present then
+    for _, tool in ipairs(M.tools) do
+      local p = mason_registry.get_package(tool)
+      if not p:is_installed() then p:install() end
+    end
+  end
 end
+
+M.setup = function()
+  require("mason").setup()
+  M.check_tools()
+  M.install_servers()
+end
+
+return M
