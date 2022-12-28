627da85bb979ad7332d8d82df9e9310a3a00ef17
haaag <haag_@tutanota.com>
2022-12-28 08:57:42 -0300

    feat(autocmd): go to last loc when opening a buffer

--- lua/me/autocmd.lua
+++ lua/me/autocmd.lua
@@ -1,17 +1,17 @@
--- [[ autocmd.lua ]]
+-- autocmd.lua
+--------------
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+local misc = vim.api.nvim_create_augroup("Misc", { clear = true })
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function() vim.highlight.on_yank() end,
   group = highlight_group,
-  pattern = '*',
+  pattern = "*",
 })
 
-
+-- Update binds when sxhkdrc is updated.
 local sxhkd_group = vim.api.nvim_create_augroup("SxhkdGroup", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
   group = sxhkd_group,
@@ -20,10 +20,36 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   desc = "Update binds when sxhkdrc is updated.",
 })
 
-local misc = vim.api.nvim_create_augroup("Misc", { clear = true })
+-- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = misc,
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "startuptime" },
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "startuptime", "netrw" },
   command = [[nnoremap <buffer><silent> q :quit<CR>]],
   desc = "Use 'q' to quit from common plugins",
 })
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPre", {
+  group = misc,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "<buffer>",
+      once = true,
+      callback = function()
+        vim.cmd(
+          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+        )
+      end,
+    })
+  end,
+})
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+--[[ local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = "packer.lua",
+  desc = 'Automatically source and re-compile packer whenever you save this init.lua'
+}) ]]
