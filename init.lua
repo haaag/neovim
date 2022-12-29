9411430d15ff55fffb144f8301dfd3eae7d41ead
haaag <haag_@tutanota.com>
2022-12-29 17:29:28 -0300

    refactor(stylua): stylua.toml update

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
--- [[ init.lua ]]
+-- init.lua
 
-require("me.packer")
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+
+require("me.lazy")
 require("me.settings")
-require("me.autocmd")
-require("me.nmaps")
-require("me.lsp")
-require("me.configs.treesitter")
-require('fidget').setup()
-require("me.configs.telescope")
-require("me.configs.cmp")
-require("me.configs.terminal")
-require("me.configs.lualine")
 
--- vim: ts=2 sts=2 sw=2 et
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("me.autocmd")
+    require("me.nmaps")
+  end,
+})
