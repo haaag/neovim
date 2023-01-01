87d8bb2868660f333feae94061d90faa4811870c
haaag <haag_@tutanota.com>
2023-01-01 10:38:30 -0300

    feat(plugin): autocompletion cmp
    indent-blankline: this plugin adds indentation guides to all lines (including empty lines).
    nvim-treesitter: treesitter configurations and abstraction layer for neovim.

---
+++ lua/me/plugins/cmp.lua
@@ -0,0 +1,123 @@
+-- cmp.lua
+-- https://github.com/hrsh7th/nvim-cmp
+
+local M = {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "onsails/lspkind-nvim",
+    "rafamadriz/friendly-snippets",
+    { "tzachar/cmp-tabnine", build = "./install.sh", dependencies = "hrsh7th/nvim-cmp" },
+  },
+}
+
+function M.config()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local lspkind = require("lspkind")
+
+  -- Set completeopt to have a better completion experience
+  vim.opt.completeopt = { "menu", "menuone", "noselect" }
+  require("luasnip.loaders.from_vscode").lazy_load()
+
+  cmp.setup({
+    sorting = {
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+
+        -- copied from cmp-under, but I don't think I need the plugin for this.
+        -- I might add some more of my own.
+        function(entry1, entry2)
+          local _, entry1_under = entry1.completion_item.label:find("^_+")
+          local _, entry2_under = entry2.completion_item.label:find("^_+")
+          entry1_under = entry1_under or 0
+          entry2_under = entry2_under or 0
+          if entry1_under > entry2_under then
+            return false
+          elseif entry1_under < entry2_under then
+            return true
+          end
+        end,
+
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    },
+
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+
+    mapping = cmp.mapping.preset.insert({
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }),
+
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "path" },
+      { name = "buffer", keyword_length = 5 },
+      { name = "cmp_tabnine" },
+    },
+
+    formatting = {
+      -- Youtube: How to set up nice formatting for your sources.
+      format = lspkind.cmp_format({
+        with_text = true,
+        menu = {
+          buffer = "[buf]",
+          nvim_lsp = "[LSP]",
+          nvim_lua = "[api]",
+          path = "[path]",
+          luasnip = "[snip]",
+          cmp_tabnine = "[TabNine]",
+        },
+      }),
+    },
+
+    --[[ experimental = {
+    -- I like the new menu better! Nice work hrsh7th
+    native_menu = false,
+
+    -- Let's play with this for a day or two
+    ghost_text = false,
+  }, ]]
+  })
+end
+
+return M
