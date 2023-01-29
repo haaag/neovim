0cc9720f8b19eae6cc51c729c0add3448e30b13d
haaag <haag_@tutanota.com>
2023-01-29 12:43:31 -0300

    refactor(treesitter): 🌲 `treesitter` config update
    - Added mini.ai for `textobjects`

--- lua/me/plugins/treesitter.lua
+++ lua/me/plugins/treesitter.lua
@@ -2,105 +2,61 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 -- Highlight, edit, and navigate code
 
-local M = {
-  "nvim-treesitter/nvim-treesitter",
-  build = function()
-    pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-  end,
-  event = "BufReadPost",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  enabled = true,
-}
-
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      "bash",
-      "c",
-      "c_sharp",
-      "cmake",
-      "cpp",
-      "css",
-      "diff",
-      "git_rebase",
-      "gitcommit",
-      "go",
-      "help",
-      "html",
-      "javascript",
-      "json",
-      "jsonc",
-      "lua",
-      "markdown",
-      "python",
-      "rasi",
-      "rust",
-      "sql",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = "BufReadPost",
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>", desc = "Schrink selection", mode = "x" },
     },
-
-    highlight = { enable = true },
-    indent = { enable = true, disable = { "python", "css" } },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<c-space>",
-        node_incremental = "<c-space>",
-        scope_incremental = "<c-s>",
-        node_decremental = "<c-backspace>",
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true, disable = { "python", "css" } },
+      ensure_installed = {
+        "bash",
+        "c",
+        "c_sharp",
+        "cmake",
+        "cpp",
+        "css",
+        "diff",
+        "git_rebase",
+        "gitcommit",
+        "go",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "python",
+        "rasi",
+        "rust",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
       },
-    },
-    textobjects = {
-      select = {
+      incremental_selection = {
         enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
         },
       },
     },
-  })
-end
-
-return M
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
