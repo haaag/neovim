6c7a49804c5612f36d56dc8aee7f3d9378e9dfc6
haaag <haag_@tutanota.com>
2023-02-07 18:35:25 -0300

    feat(treesitter): 🔌 changed text-objects plug-in
    - Testing `nvim-treesitter-textobjects`
    - Removed `mini.ai`

--- lua/me/plugins/treesitter.lua
+++ lua/me/plugins/treesitter.lua
@@ -12,6 +12,9 @@ return {
       { "<c-space>", desc = "Increment selection" },
       { "<bs>", desc = "Schrink selection", mode = "x" },
     },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     ---@type TSConfig
     opts = {
       highlight = { enable = true },
@@ -51,6 +54,61 @@ return {
           node_incremental = "<C-space>",
           scope_incremental = "<nop>",
           node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        enable = true,
+        swap = {
+          enable = false,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]p"] = "@parameter.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]P"] = "@parameter.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[p"] = "@parameter.outer",
+            ["[c"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[P"] = "@parameter.outer",
+            ["[C"] = "@class.outer",
+          },
+        },
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+
+            ["ad"] = "@conditional.outer",
+            ["id"] = "@conditional.inner",
+
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+          },
         },
       },
     },
