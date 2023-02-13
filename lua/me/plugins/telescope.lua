27723088d8ba92d946d521fb259f84beb4d64454
haaag <haag_@tutanota.com>
2023-02-13 18:26:58 -0300

    refactor(telescope): 📦 `telescope-nvim` config update

--- lua/me/plugins/telescope.lua
+++ lua/me/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- telescope.lua
 -- https://github.com/nvim-telescope/telescope.nvim
 
-local M = {
+return {
   "nvim-telescope/telescope.nvim",
   event = "VeryLazy",
   version = "0.1.x",
@@ -11,75 +11,58 @@ local M = {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     { "nvim-telescope/telescope-project.nvim" }, -- https://github.com/nvim-telescope/telescope-project.nvim
-    { "debugloop/telescope-undo.nvim" }, -- https://github.com/debugloop/telescope-undo.nvim
-    { "nvim-telescope/telescope-dap.nvim" },
+    { "nvim-telescope/telescope-frecency.nvim", dependencies = { "kkharji/sqlite.lua" } },
+    -- { "debugloop/telescope-undo.nvim" }, -- https://github.com/debugloop/telescope-undo.nvim
+    -- { "nvim-telescope/telescope-dap.nvim" },
   },
+    -- stylua: ignore
+    -- luacheck: ignore
   keys = {
-    { "<leader>?", "<CMD>Telescope oldfiles<CR>", desc = "[?] Find recently opened files" },
+    -- search
     { "<C-p>", "<CMD>Telescope find_files<CR>", desc = "Search Files" },
+    -- { "<C-p>", "<CMD>Telescope find_files theme=ivy<CR>", desc = "Search Files" },
+    {
+      "<C-b>",
+      "<CMD>lua require('telescope.builtin').buffers(require('telescope.themes').get_cursor({ previewer = false }))<CR>",
+      desc = "[S]earch Buffers",
+    },
+    { "<leader><leader>", "<CMD>lua require('telescope').extensions.frecency.frecency({ workspace = 'CWD' })<CR>", desc = "Telescope frecency", },
+    { "<leader>?", "<CMD>Telescope oldfiles<CR>", desc = "[?] Find recently opened files" },
+    { "<leader><leader>?", "<CMD>lua require('telescope').extensions.frecency.frecency()<CR>", desc = "[?] Find recently opened (frecency)", },
     { "<leader>sp", "<CMD>Telescope project<CR>", desc = "[S]earch [P]roject" },
     { "<leader>sr", "<CMD>Telescope resume<CR>", desc = "[S]earch [R]esume" },
     { "<leader>sw", "<CMD>Telescope grep_string<CR>", desc = "[S]earch current [W]ord" },
     { "<leader>sg", "<CMD>Telescope live_grep<CR>", desc = "[S]earch by [G]rep" },
     { "<leader>sd", "<CMD>Telescope diagnostics<CR>", desc = "[S]earch [D]iagnostics" },
-    { "<leader>gc", "<CMD>Telescope git_commits<CR>", desc = "[G]it Telescope [C]ommits" },
-    { "<leader>gb", "<CMD>Telescope git_branches<CR>", desc = "[G]it Telescope [B]ranches" },
     { "<leader>sh", "<CMD>Telescope help_tags<CR>", desc = "[S]earch [H]elp" },
     { "<leader>su", "<CMD>Telescope undo<CR>", desc = "[S]earch [U]ndo" },
+    { "<leader>sk", "<CMD>Telescope keymaps<CR>", desc = "[S]earch [K]eymaps" },
+    { "<leader>:", "<CMD>Telescope command_history<CR>", desc = "[S]earch Command History" },
     {
       "<leader><space>",
-      function()
-        require("telescope.builtin").buffers(require("telescope.themes").get_dropdown({
-          previewer = false,
-        }))
-      end,
-      desc = "[S]earch [U]ndo",
+      "<CMD>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown({ previewer = false }))<CR>",
+      desc = "[S]earch Buffers",
     },
     {
       "<leader>/",
-      function()
-        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_ivy({
-          winblend = 10,
-          previewer = false,
-        }))
-      end,
+      "<CMD>lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_ivy({winblend = 10, previewer = false}))<CR>",
       desc = "[/] Fuzzily search in current buffer]",
     },
-    { "<leader>dt", "<CMD>Telescope dap commands<CR>", desc = "" },
-    { "<leader>dt", "<CMD>Telescope dap configurations<CR>", desc = "" },
-    { "<leader>dt", "<CMD>Telescope dap list_breakpoints<CR>", desc = "" },
-    { "<leader>dt", "<CMD>Telescope dap variables<CR>", desc = "" },
-    { "<leader>dt", "<CMD>Telescope dap frames<CR>", desc = "" },
   },
+  init = function()
+    -- Enable telescope fzf native
+    pcall(require("telescope").load_extension, "fzf")
+
+    -- Enable telescope undo
+    -- pcall(require("telescope").load_extension, "undo")
+
+    -- Enable telescope project
+    pcall(require("telescope").load_extension, "project")
+
+    -- Enable frecency
+    pcall(require("telescope").load_extension, "frecency")
+
+    -- Enable telescope dap
+    -- pcall(require("telescope").load_extension, "dap")
+  end,
 }
-
-function M.config()
-  local telescope = require("telescope")
-  local actions = require("telescope.actions")
-
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-u>"] = false,
-          ["<C-d>"] = false,
-          ["<esc>"] = actions.close,
-        },
-      },
-    },
-  })
-
-  -- Enable telescope fzf native
-  pcall(require("telescope").load_extension, "fzf")
-
-  -- Enable telescope undo
-  pcall(require("telescope").load_extension, "undo")
-
-  -- Enable telescope project
-  pcall(require("telescope").load_extension, "project")
-
-  -- Enable telescope dap
-  pcall(require("telescope").load_extension, "dap")
-end
-
-return M
