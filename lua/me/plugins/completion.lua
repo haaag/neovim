0b0b646288c18f12af6cc881d6e5b3efa4940b25
haaag <81921095+haaag@users.noreply.github.com>
2023-06-08 17:52:55 -0300

    feat(surround): ⌨️ add `mini.surround`
    replace:
    `kylechui/nvim-surround` --> `echasnovski/mini.surround`

--- lua/me/plugins/completion.lua
+++ lua/me/plugins/completion.lua
@@ -1,24 +1,38 @@
 return {
 
-  { -- https://github.com/kylechui/nvim-surround
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = true,
+  { -- https://github.com/echasnovski/mini.surround
+    "echasnovski/mini.surround",
+    keys = function(plugin, keys)
+      -- Populate the keys based on the user's options
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local mappings = {
+        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "Delete surrounding" },
+        { opts.mappings.find, desc = "Find right surrounding" },
+        { opts.mappings.find_left, desc = "Find left surrounding" },
+        { opts.mappings.highlight, desc = "Highlight surrounding" },
+        { opts.mappings.replace, desc = "Replace surrounding" },
+        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+      return vim.list_extend(mappings, keys)
+    end,
+    version = false,
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      require("mini.surround").setup(opts)
+    end,
     enabled = true,
   },
-
-  -- {
-  --   "echasnovski/mini.pairs",
-  --   event = "VeryLazy",
-  --   config = function(_, opts)
-  --     require("mini.pairs").setup(opts)
-  --   end,
-  -- },
-  -- {
-  --   "windwp/nvim-autopairs",
-  --   config = true,
-  -- },
 
   { -- https://github.com/ms-jpq/coq_nvim
     "ms-jpq/coq_nvim",
