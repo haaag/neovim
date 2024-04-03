64a13e117574fc5571970cfe6298495219199578
haaag <81921095+haaag@users.noreply.github.com>
2024-04-03 11:36:46 -0300

    feat(lsp): add `basedpyright`
    - testing new pyright fork

---
+++ lua/me/plugins/lsp/servers/basedpyright.lua
@@ -0,0 +1,35 @@
+-- lsp.servers.pyright
+return {
+  { -- https://github.com/neovim/nvim-lspconfig
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        basedpyright = {
+          autostart = true,
+          disableOrganizeImports = true,
+          handlers = { ['textDocument/publishDiagnostics'] = function() end },
+          on_attach = function(client, _)
+            client.server_capabilities.codeActionProvider = false
+          end,
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                typeCheckingMode = 'off',
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'ninja', 'python', 'rst', 'toml' })
+      end
+    end,
+  },
+}
